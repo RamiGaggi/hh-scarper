4bdb20e9ba9d2fe18b73aab02bb60439423c792e
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-08-24 23:38:02 +0300

    Test refactoring

--- hhscarper/tests/test_scrape.py
+++ hhscarper/tests/test_scrape.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import pytest
 import requests_mock
 from dotenv import load_dotenv
-from hhscarper.models import Request, VacancyRequest
+from hhscarper.models import Request, Vacancy, VacancyRequest
 from hhscarper.scarper import scrape
 
 load_dotenv()
@@ -15,25 +15,43 @@ KEYWORD = 'test_keyword'
 URL = 'https://api-test.hhscarpertest.ru/vacancies'
 URL_KEYWWORD = f'{URL}?per_page=100&text={KEYWORD}'
 PAGE_URL = f'{URL}?per_page=100&text={KEYWORD}&page=0'
-VACANCY_ID1 = 000
-VACANCY_ID2 = 777
-VACANCY_URL1 = f'{URL}/{VACANCY_ID1}'
-VACANCY_URL2 = f'{URL}/{VACANCY_ID2}'
+
+vacancies = {
+    'vacancy_id1': 111,
+    'vacancy_id2': 222,
+    'vacancy_id3': 333,
+    'vacancy_id4': 000,
+}
+
+VACANCY_URL1 = f'{URL}/{vacancies["vacancy_id1"]}'
+VACANCY_URL2 = f'{URL}/{vacancies["vacancy_id2"]}'
+VACANCY_URL3 = f'{URL}/{vacancies["vacancy_id3"]}'
+VACANCY_URL4 = f'{URL}/{vacancies["vacancy_id4"]}'
 
 
 @pytest.fixture
-def requests():
+def initial():
     time = datetime.now().time()
     Request.objects.create(
         keyword=KEYWORD,
         status='pending',
         time=time,
     )
+    Vacancy.objects.create(
+        external_id=333,
+        title='Existed vacancy',
+        link='https://xa.com',
+        description='Existed vacancy',
+        key_skills=['1', '2', '3'],
+        lemmas=['4', '5', '6'],
+    )
 
 
 @pytest.mark.django_db
-def test_scrape(client, requests):
-    req_obj = Request.objects.get(keyword=KEYWORD)
+def test_scrape(client, initial):
+    assert Request.objects.count() == 1
+    assert Vacancy.objects.count() == 1
+
     with requests_mock.Mocker() as mock:
         mock.get(
             URL_KEYWWORD,
@@ -45,37 +63,58 @@ def test_scrape(client, requests):
         mock.get(
             PAGE_URL,
             json={
-                'items': [{'id': 000}, {'id': 777}],
+                'items': [
+                    {'id': vacancy_id} for vacancy_id in vacancies.values()
+                ],
             },
         )
         mock.get(VACANCY_URL1, json={
-            'id': 000,
+            'id': vacancies['vacancy_id1'],
             'name': 'Some title',
             'description': 'Magic test',
             'key_skills': [{'name': 'Postgres'}],
             'alternate_url': 'https://test1.test',
         })
         mock.get(VACANCY_URL2, json={
-            'id': 777,
+            'id': vacancies['vacancy_id2'],
             'name': 'Another title',
             'description': 'Magic test2',
             'key_skills': [],
             'alternate_url': 'https://tes2.test',
         })
+        mock.get(VACANCY_URL3, json={
+            'id': vacancies['vacancy_id3'],
+            'name': 'Another title',
+            'description': 'Magic test2',
+            'key_skills': [],
+            'alternate_url': 'https://tes2.test',
+        })
+        mock.get(VACANCY_URL4, json={
+            'id': vacancies['vacancy_id4'],
+            'name': 'Another title',
+            'description': 'Magic test2',
+            'key_skills': [],
+            'alternate_url': 'https://tes2.test',
+        })
+    # Actual testing
+        req_obj = Request.objects.get(keyword=KEYWORD)
         scrape_result = scrape(
             keyword=KEYWORD,
             request_obj_id=req_obj.pk,
             adress='https://api-test.hhscarpertest.ru/vacancies',
         )
 
-    vacancy_object = req_obj.vacancy_set.get(external_id=000)
+    vacancy_object = req_obj.vacancy_set.get(external_id=111)
 
-    assert Request.objects.all().count() == 1
-    assert VacancyRequest.objects.all().count() == 2
-    assert req_obj.vacancy_set
+    assert VacancyRequest.objects.count() == 4
+    assert req_obj.vacancy_set.count() == 4
+    assert Request.objects.count() == 1
+    assert Request.objects.count() == 1
+    assert scrape_result == 4
 
     assert vacancy_object.title == 'Some title'
     assert vacancy_object.link == 'https://test1.test'
     assert vacancy_object.description == 'Magic test'
-    assert vacancy_object.key_skills == ['Postgres']
-    assert scrape_result == 2
+    assert 'Postgres' in vacancy_object.key_skills
+    assert 'MAGIC' in vacancy_object.lemmas
+    assert 'TEST' in vacancy_object.lemmas
--- hhscarper/tests/test_views.py
+++ hhscarper/tests/test_views.py
@@ -1,9 +1,46 @@
+from datetime import datetime
+
 import pytest
 from django.urls import reverse
+from hhscarper.models import Request
+
+
+@pytest.fixture
+def initial():
+    time = datetime.now().time()
+    Request.objects.create(
+        keyword='KEYWORD',
+        status='pending',
+        time=time,
+    )
 
 
 @pytest.mark.django_db
-def test_list(client):
+def test_request_list(client):
     url = reverse('hhscarper:request-list')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_request_detail(client, initial):
+    url = reverse(
+        'hhscarper:request-detail',
+        kwargs={'pk': 1},
+    )
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_dashboard(client):
+    url = reverse('hhscarper:dashboard')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_vacancy_list(client):
+    url = reverse('hhscarper:vacancy-list')
     response = client.get(url)
     assert response.status_code == 200
