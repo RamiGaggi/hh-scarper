8a8fa4f79bc6127e88ed6dbd9b75a93eef48561f
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-08-28 13:13:00 +0300

    Add test forreport deatil view

--- hhscarper/tests/test_views.py
+++ hhscarper/tests/test_views.py
@@ -2,16 +2,29 @@ from datetime import datetime
 
 import pytest
 from django.urls import reverse
-from hhscarper.models import Request
+from hhscarper.models import Request, SkillReport, WordReport
 
 
 @pytest.fixture
 def initial():
     time = datetime.now().time()
     Request.objects.create(
-        keyword='KEYWORD',
-        status='pending',
+        keyword='KEYWORD_PENDING',
+        status='Pending',
         time=time,
+    )
+    Request.objects.create(
+        keyword='KEYWORD_RESOLVED',
+        status='Resolved',
+        time=time,
+    )
+    SkillReport.objects.create(
+        data={'skill': 14, 'bar': 1, 'zax': 4},
+        request=Request.objects.get(keyword='KEYWORD_RESOLVED'),
+    )
+    WordReport.objects.create(
+        data={'word': 12, 'bar': 2, 'vax': 5},
+        request=Request.objects.get(keyword='KEYWORD_RESOLVED'),
     )
 
 
@@ -26,10 +39,16 @@ def test_request_list(client):
 def test_request_detail(client, initial):
     url = reverse(
         'hhscarper:request-detail',
-        kwargs={'pk': Request.objects.get(keyword='KEYWORD').pk},
+        kwargs={'pk': Request.objects.get(keyword='KEYWORD_PENDING').pk},
     )
-    response = client.get(url)
-    assert response.status_code == 200
+    response_pending = client.get(url)
+    assert response_pending.status_code == 302
+    url = reverse(
+        'hhscarper:request-detail',
+        kwargs={'pk': Request.objects.get(keyword='KEYWORD_RESOLVED').pk},
+    )
+    response_resolved = client.get(url)
+    assert response_resolved.status_code == 200
 
 
 @pytest.mark.django_db
