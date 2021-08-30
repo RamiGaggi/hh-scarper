3b693b7a0ec9d0accd8506c9c767bd3717a05455
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-08-30 19:23:58 +0300

    Add reqauests filter for vacancy

--- hhscarper/filters.py
+++ hhscarper/filters.py
@@ -12,8 +12,18 @@ class RequestFilter(django_filters.FilterSet):
 
 
 class VacancyFilter(django_filters.FilterSet):
+
+    def __init__(self, *args, **kwargs):
+        """Update filters after creating status/labels."""
+        super().__init__(*args, **kwargs)
+        self.filters['requests'].extra['choices'] = sorted(
+            ((obj.pk, obj.keyword) for obj in Request.objects.all()),
+            key=lambda request: request[1].lower(),
+        )
+
     title = django_filters.CharFilter(lookup_expr='icontains', label=_('Название вакансии'))
+    requests = django_filters.TypedChoiceFilter(label=_('Запросы'))
 
     class Meta:
         model = Vacancy
-        fields = ['title']
+        fields = ['title', 'requests']
